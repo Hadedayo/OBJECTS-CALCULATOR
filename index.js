bf15f76c376ff73f6f1d855016b0192fcdf079c0
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-10-10 22:24:10 +0100

    Deleted name function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Calculator= {
   this.subtract: "subtract";
   this.multiply:"multiply";
   this.divide: "divide";
-  fullname: function () {
-    return this.firstname + "" + this.lastname;
+
   }
 }
+ Calculator.add();
