a6b3d3522a602b85d969e9b45dd26f1691a14b32
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-10-10 21:57:16 +0100

    Included an object called Calculator

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var Calculator= {
+  this.add: "add";
+  this.subtract: "subtract";
+  this.multiply:"multiply";
+  this.divide: "divide";
+  fullname: function () {
+    return this.firstname + "" + this.lastname;
+  }
+}
