77d5f3f4e1c57027a1a635eea28ae3b5c35b24fd
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-10-11 09:27:11 +0100

    Added the (+ - * /) functions

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 
-var Calculator= {
+var Calculator = function (add, subtract, multiply, divide) {
   this.add: "add";
   this.subtract: "subtract";
   this.multiply:"multiply";
   this.divide: "divide";
+}
+prompt("Please enter a value1");
+prompt("Please enter another value2");
+
+function add(num1, num2) {
+  return (num1+num2);
+  if (true) {
 
   }
 }
- Calculator.add();
+
+function subtract(num1, num2) {
+  return (num1-num2);
+}
+
+function multiply(num1, num2) {
+  return (num1*num2);
+}
+
+function divide(num1, num2) {
+  return (num1/num2);
+}
